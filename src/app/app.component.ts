a2f48bd3f529df085ed6fba63174de9c1f32ed14
Adones Pitogo <pitogo.adones@gmail.com>
2016-10-26 13:09:59 +0800

    default route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 export class AppComponent {
   title = 'UNSAKINI';
 
-  IsConstructor(
+  constructor(
     private router: Router
   ) {
     this.router.navigate('/dashboard');
