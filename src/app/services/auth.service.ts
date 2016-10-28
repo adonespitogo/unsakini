31ae88b50854a0f185441392d4607c8ff5880f24
Adones Pitogo <pitogo.adones@gmail.com>
2016-10-28 22:42:59 +0800

    base64 encryption key

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,15 @@
+
+// import {UserModel} from '../models/user.model';
+
+// export class AuthService {
+
+//   public static currentUser: UserModel;
+//   protected encryptionKey: string;
+
+//   public static setEncryptionkey
+
+//   public static getAuthKey (): string {
+
+//   }
+
+// }
