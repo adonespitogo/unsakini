b9eae2bd17897621177d881a84dc56bbcf8bba71
Adones Pitogo <pitogo.adones@gmail.com>
2016-11-01 08:27:15 +0800

    bootstrap ng app on document.ready

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,14 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { AppModule } from './app/';
+import * as $ from 'jquery';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+
+$(document).ready( () => {
+  $("#footer").show();
+  platformBrowserDynamic().bootstrapModule(AppModule);
+});
