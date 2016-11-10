4c5603be665823145bb5a471c642b964302ad6e2
Adones Pitogo <pitogo.adones@gmail.com>
2016-11-10 20:29:23 +0800

    init config must be before migration call

--- postinstall.js
+++ postinstall.js
@@ -4,6 +4,6 @@ var env = process.env.NODE_ENV || 'development'
 
 exec('typings install');
 exec('npm rebuild node-sass');
+exec('gulp init:config');
 exec('node db-migrate.js');
 exec('ng build ' + (env === 'production' ? '--prod' : '') );
-exec('gulp init:config');
