6036d568bcffeb03dd25ce20fc890d8c308c641a
Adones Pitogo <pitogo.adones@gmail.com>
2016-11-16 20:17:54 +0800

    created board model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161115161436) do
+ActiveRecord::Schema.define(version: 20161116114222) do
+
+  create_table "boards", force: :cascade do |t|
+    t.text     "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "provider",               default: "email", null: false
---
+++ spec/models/board_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Board, type: :model do
+
+  describe 'Board Model' do
+    it 'encrypts name' do
+      
+    end
+  end
+
+end
