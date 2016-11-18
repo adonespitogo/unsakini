f0e6b3ce7e755709cf6d6ca64e102404eec34ee6
Adones Pitogo <pitogo.adones@gmail.com>
2016-11-18 11:41:12 +0800

    created post model and spec

---
+++ spec/factories/posts.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :post do
+    title "MyText"
+    content "MyText"
+    user_id 1
+    board_id 1
+  end
+end
---
+++ spec/models/post_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+require 'models/concerns/encryptable_concern'
+
+RSpec.describe Post, type: :model do
+
+  describe 'Post Model' do
+    it_behaves_like 'encryptable'
+  end
+
+end
