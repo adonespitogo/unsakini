2f10b8846aea46a39d1939d1e816c2bb27136083
Adones Pitogo <pitogo.adones@gmail.com>
2016-11-18 16:12:27 +0800

    fix before_action

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -2,8 +2,8 @@ class Api::PostsController < ApplicationController
   include BoardOwnerControllerConcern
   include PostOwnerControllerConcern
 
-  before_action :ensure_board_set, only: [:index]
-  before_action :is_post_owner, except: [:index]
+  before_action :ensure_board_set, only: [:index, :create]
+  before_action :is_post_owner, only: [:show]
 
 # Renders the post belonging to the board
 #
@@ -17,6 +17,13 @@ class Api::PostsController < ApplicationController
 # `GET /api/boards/:board_id/posts/id`
   def show
     render json: @post
+  end
+
+# Creates a single post belonging to the board
+#
+# `POST /api/boards/:board_id/posts/`
+  def create
+    render status: :ok
   end
 
 end
--- spec/requests/api/board/api_board_posts_spec.rb
+++ spec/requests/api/board/api_board_posts_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Api::Board::Posts", type: :request do
 
-  before(:all) do
+  before(:each) do
     @user = create :user
     @board = create :board
     @user_board = create(:user_board, {
@@ -61,7 +61,33 @@ RSpec.describe "Api::Board::Posts", type: :request do
     end
     it "return board posts" do
       get api_board_post_path(@board, @post), headers: auth_headers(@user)
+      expect(response).to have_http_status(:ok)
       expect(body_as_hash).to equal_model_hash(@post)
+    end
+  end
+
+  describe "POST /api_board_posts" do
+    let(:valid_attributes) {
+      {title: "my title", content: "my conetent"}
+    }
+    let(:invalid_title_attribute) {
+      {title: "", content: "asdfadfa"}
+    }
+    let(:invalid_content_attribute) {
+      {title: "afadsf", content: ""}
+    }
+
+    it "return http unauthorized" do
+      post api_board_posts_path(@board), as: :json
+      expect(response).to have_http_status(:unauthorized)
+    end
+    it "return http unauthorized" do
+      post api_board_posts_path(@other_board), headers: auth_headers(@user), params: valid_attributes, as: :json
+      expect(response).to have_http_status(:unauthorized)
+    end
+    it "return http unprocessable_entity" do
+      post api_board_posts_path(@board), headers: auth_headers(@user), params: invalid_title_attribute, as: :json
+      expect(response).to have_http_status(:unprocessable_entity)
     end
   end
 end
