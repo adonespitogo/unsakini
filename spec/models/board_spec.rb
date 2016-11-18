2af597a4d8ba98a701c7d3a63524912bed958ea5
Adones Pitogo <pitogo.adones@gmail.com>
2016-11-18 19:59:53 +0800

    test for encryptable

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
-require 'models/concerns/encryptable_concern'
 
 RSpec.describe Board, type: :model do
   it_behaves_like 'encryptable'
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
-require 'models/concerns/encryptable_concern'
 
 RSpec.describe Post, type: :model do
   it_behaves_like 'encryptable'
