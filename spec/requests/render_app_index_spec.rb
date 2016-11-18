125d357186c1195d6417ef284eb9b583e8fcb132
Adones Pitogo <pitogo.adones@gmail.com>
2016-11-18 20:16:23 +0800

    refactor render app index view

--- spec/requests/render_app_index_spec.rb
+++ spec/requests/render_app_index_spec.rb
@@ -1,8 +1,18 @@
 require 'rails_helper'
 
-describe 'catch html5 pushState routes' do
-  it 'get /app/any renders app/index.html' do
-    get '/app/any'
-    expect(response.body).to match File.read(Rails.public_path.join("app","index.html"))
+RSpec.describe "WebBaseController", type: :request do
+  describe 'catch html5 pushState routes' do
+
+    def self.visit_app_urls(urls)
+      urls.each do |url|
+        it "renders app/index.html when visiting #{url}" do
+          get "/#{url}"
+          expect(response.body).to match File.read(Rails.public_path.join("app","index.html"))
+        end
+      end
+    end
+
+    visit_app_urls(['/app', 'app/', 'app/*anything'])
+
   end
 end
--- spec/support/request_helpers.rb
+++ spec/support/request_helpers.rb
@@ -1,5 +1,5 @@
 module RequestHelpers
 
-
+  
 
 end
