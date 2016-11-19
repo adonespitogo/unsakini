2552964c012ecbd80e0f43ebd53bd3716d4b86a8
Adones Pitogo <pitogo.adones@gmail.com>
2016-11-20 00:22:33 +0800

    clean up a bit

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -56,5 +56,8 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
+  config.include JsonSpec::Helpers
+  config.include FactoryGirl::Syntax::Methods
+  config.include Devise::Test::ControllerHelpers, type: :controller
 
 end
--- spec/support/helper_gems.rb
+++
@@ -1,5 +0,0 @@
-RSpec.configure do |config|
-  config.include JsonSpec::Helpers
-  config.include FactoryGirl::Syntax::Methods
-  config.include Devise::Test::ControllerHelpers, type: :controller
-end
