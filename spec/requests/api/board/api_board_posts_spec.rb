cd269a751e5a541772c2ebfd6116aa41d2e92004
Adones Pitogo <pitogo.adones@gmail.com>
2016-11-20 02:44:57 +0800

    done posts spec

--- spec/requests/api/board/api_board_posts_spec.rb
+++ spec/requests/api/board/api_board_posts_spec.rb
@@ -5,10 +5,6 @@ RSpec.describe "Api::Board::Posts", type: :request do
 
   before(:all) do
     Timecop.freeze
-    @user = create(:user)
-  end
-
-  before(:each) do
     user_has_shared_board_scenario
   end
 
@@ -71,10 +67,12 @@ RSpec.describe "Api::Board::Posts", type: :request do
       it "return http unprocessable_entity when invalid title" do
         post api_board_posts_path(@board), headers: auth_headers(@user), params: invalid_title_attribute, as: :json
         expect(response).to have_http_status(:unprocessable_entity)
+        # todo: assert errors
       end
       it "return http unprocessable_entity when invalid content" do
         post api_board_posts_path(@board), headers: auth_headers(@user), params: invalid_content_attribute, as: :json
         expect(response).to have_http_status(:unprocessable_entity)
+        # todo: assert errors
       end
       it "successfully creates a post" do
         board_posts_count = @board.posts.count
@@ -99,10 +97,12 @@ RSpec.describe "Api::Board::Posts", type: :request do
       it "return http unprocessable_entity when invalid title" do
         put api_board_post_path(@board, @post), headers: auth_headers(@user), params: invalid_title_attribute, as: :json
         expect(response).to have_http_status(:unprocessable_entity)
+        # todo: assert errors
       end
       it "return http unprocessable_entity when invalid content" do
         put api_board_post_path(@board, @post), headers: auth_headers(@user), params: invalid_content_attribute, as: :json
         expect(response).to have_http_status(:unprocessable_entity)
+        # todo: assert errors
       end
       it "updates my post belonging to my board" do
         put api_board_post_path(@board, @post), headers: auth_headers(@user), params: valid_attributes, as: :json
@@ -132,151 +132,167 @@ RSpec.describe "Api::Board::Posts", type: :request do
     end
   end
 
-  # context "Shared Board Posts" do
+  context "Shared Board Posts" do
 
-  #   describe "Get all posts" do
-  #     it "return http unauthorized" do
-  #       get api_board_posts_path(@shared_board)
-  #       expect(response).to have_http_status(:unauthorized)
-  #     end
-  #     it "returns all posts for first user" do
-  #       get api_board_posts_path(@shared_board), headers: auth_headers(@user)
-  #       expect(response).to have_http_status(:ok)
-  #       expect(body_as_hash).to equal_model_hash(@shared_board.posts.all)
-  #     end
-  #     it "returns all posts for second user" do
-  #       get api_board_posts_path(@shared_board), headers: auth_headers(@user_2)
-  #       expect(response).to have_http_status(:ok)
-  #       expect(body_as_hash).to equal_model_hash(@shared_board.posts.all)
-  #     end
-  #   end
+    describe "Get all posts" do
+      it "return http unauthorized" do
+        get api_board_posts_path(@shared_board)
+        expect(response).to have_http_status(:unauthorized)
+      end
+      it "returns all posts for first user" do
+        get api_board_posts_path(@shared_board), headers: auth_headers(@user)
+        expect(response).to have_http_status(:ok)
+        expect(parse_json(response.body, '0')).to match_json_schema(:post)
+        expect(response.body).to be_json_eql(serialize(@shared_board.posts.all))
+      end
+      it "returns all posts for second user" do
+        get api_board_posts_path(@shared_board), headers: auth_headers(@user_2)
+        expect(response).to have_http_status(:ok)
+        expect(parse_json(response.body, '0')).to match_json_schema(:post)
+        expect(response.body).to be_json_eql(serialize(@shared_board.posts.all))
+      end
+    end
 
-  #   describe "Get single post" do
-  #     it "return http unauthorized" do
-  #       get api_board_post_path(@shared_board, @shared_post)
-  #       expect(response).to have_http_status(:unauthorized)
-  #     end
-  #     it "renders post resource to first user" do
-  #       get api_board_post_path(@shared_board, @shared_post), headers: auth_headers(@user)
-  #       expect(response).to have_http_status(:ok)
-  #       expect(body_as_hash).to equal_model_hash(@shared_post)
-  #     end
-  #     it "renders post resource to 2nd user" do
-  #       get api_board_post_path(@shared_board, @shared_post), headers: auth_headers(@user_2)
-  #       expect(response).to have_http_status(:ok)
-  #       expect(body_as_hash).to equal_model_hash(@shared_post)
-  #     end
+    describe "Get single post" do
+      it "return http unauthorized" do
+        get api_board_post_path(@shared_board, @shared_post)
+        expect(response).to have_http_status(:unauthorized)
+      end
+      it "renders post resource to first user" do
+        get api_board_post_path(@shared_board, @shared_post), headers: auth_headers(@user)
+        expect(response).to have_http_status(:ok)
+        expect(response.body).to match_json_schema(:post)
+        expect(response.body).to be_json_eql(serialize(@shared_post))
+      end
+      it "renders post resource to 2nd user" do
+        get api_board_post_path(@shared_board, @shared_post), headers: auth_headers(@user_2)
+        expect(response).to have_http_status(:ok)
+        expect(response.body).to match_json_schema(:post)
+        expect(response.body).to be_json_eql(serialize(@shared_post))
+      end
+    end
 
-  #     describe "Create post" do
 
-  #       it "return http unauthorized" do
-  #         post api_board_posts_path(@shared_board), as: :json
-  #         expect(response).to have_http_status(:unauthorized)
-  #       end
+    describe "Create post" do
 
-  #       describe "Board owner" do
-  #         it "return http unprocessable_entity when invalid title" do
-  #           post api_board_posts_path(@shared_board), headers: auth_headers(@user), params: invalid_title_attribute, as: :json
-  #           expect(response).to have_http_status(:unprocessable_entity)
-  #         end
-  #         it "return http unprocessable_entity when invalid content" do
-  #           post api_board_posts_path(@shared_board), headers: auth_headers(@user), params: invalid_content_attribute, as: :json
-  #           expect(response).to have_http_status(:unprocessable_entity)
-  #         end
-  #         it "successfully creates a post" do
-  #           board_posts_count = @shared_board.posts.count
-  #           post api_board_posts_path(@shared_board), headers: auth_headers(@user), params: valid_attributes, as: :json
-  #           expect(response).to have_http_status(:created)
-  #           expect(body_as_hash).to equal_model_hash(@shared_board.posts.last)
-  #           expect(@shared_board.posts.count).to eq(board_posts_count+1)
-  #         end
-  #       end
+      it "return http unauthorized" do
+        post api_board_posts_path(@shared_board), as: :json
+        expect(response).to have_http_status(:unauthorized)
+      end
 
-  #       describe "Board shared user" do
-  #         it "return http unprocessable_entity when invalid title" do
-  #           post api_board_posts_path(@shared_board), headers: auth_headers(@user_2), params: invalid_title_attribute, as: :json
-  #           expect(response).to have_http_status(:unprocessable_entity)
-  #         end
-  #         it "return http unprocessable_entity when invalid content" do
-  #           post api_board_posts_path(@shared_board), headers: auth_headers(@user_2), params: invalid_content_attribute, as: :json
-  #           expect(response).to have_http_status(:unprocessable_entity)
-  #         end
-  #         it "successfully creates a post" do
-  #           board_posts_count = @shared_board.posts.count
-  #           post api_board_posts_path(@shared_board), headers: auth_headers(@user_2), params: valid_attributes, as: :json
-  #           expect(response).to have_http_status(:created)
-  #           expect(body_as_hash).to equal_model_hash(@shared_board.posts.last)
-  #           expect(@shared_board.posts.count).to eq(board_posts_count+1)
-  #         end
-  #       end
+      context "Board owner" do
+        it "return http unprocessable_entity when invalid title" do
+          post api_board_posts_path(@shared_board), headers: auth_headers(@user), params: invalid_title_attribute, as: :json
+          expect(response).to have_http_status(:unprocessable_entity)
+          # todo: assert errors
+        end
+        it "return http unprocessable_entity when invalid content" do
+          post api_board_posts_path(@shared_board), headers: auth_headers(@user), params: invalid_content_attribute, as: :json
+          expect(response).to have_http_status(:unprocessable_entity)
+          # todo: assert errors
+        end
+        it "successfully creates a post" do
+          board_posts_count = @shared_board.posts.count
+          post api_board_posts_path(@shared_board), headers: auth_headers(@user), params: valid_attributes, as: :json
+          expect(response).to have_http_status(:created)
+          expect(response.body).to match_json_schema(:post)
+          expect(response.body).to be_json_eql(serialize(@shared_board.posts.last))
+          expect(@shared_board.posts.count).to eq(board_posts_count+1)
+        end
+      end
 
-  #     end
+      context "Board shared user" do
+        it "return http unprocessable_entity when invalid title" do
+          post api_board_posts_path(@shared_board), headers: auth_headers(@user_2), params: invalid_title_attribute, as: :json
+          expect(response).to have_http_status(:unprocessable_entity)
+          # todo: assert errors
+        end
+        it "return http unprocessable_entity when invalid content" do
+          post api_board_posts_path(@shared_board), headers: auth_headers(@user_2), params: invalid_content_attribute, as: :json
+          expect(response).to have_http_status(:unprocessable_entity)
+          # todo: assert errors
+        end
+        it "successfully creates a post" do
+          board_posts_count = @shared_board.posts.count
+          post api_board_posts_path(@shared_board), headers: auth_headers(@user_2), params: valid_attributes, as: :json
+          expect(response).to have_http_status(:created)
+          expect(response.body).to match_json_schema(:post)
+          expect(response.body).to be_json_eql(serialize(@shared_board.posts.last))
+          expect(@shared_board.posts.count).to eq(board_posts_count+1)
+        end
+      end
 
-  #     describe "Update my post" do
+    end
 
-  #       # owner of the post in the board should be able to update it
-  #       it "return http unauthorized" do
-  #         put api_board_post_path(@shared_board, @shared_post), as: :json
-  #         expect(response).to have_http_status(:unauthorized)
-  #       end
+    describe "Update my post" do
 
-  #       describe "Post owner" do
-  #         it "return http unprocessable_entity when invalid title" do
-  #           put api_board_post_path(@shared_board, @shared_post), headers: auth_headers(@user), params: invalid_title_attribute, as: :json
-  #           expect(response).to have_http_status(:unprocessable_entity)
-  #         end
-  #         it "return http unprocessable_entity when invalid content" do
-  #           put api_board_post_path(@shared_board, @shared_post), headers: auth_headers(@user), params: invalid_content_attribute, as: :json
-  #           expect(response).to have_http_status(:unprocessable_entity)
-  #         end
-  #         it "updates my post belonging to my board" do
-  #           put api_board_post_path(@shared_board, @shared_post), headers: auth_headers(@user), params: valid_attributes, as: :json
-  #           expect(response).to have_http_status(:ok)
-  #           expect(body_as_hash).to equal_model_hash(@shared_post.reload)
-  #           expect(body_as_hash[:title]).to eq(valid_attributes[:title])
-  #           expect(body_as_hash[:content]).to eq(valid_attributes[:content])
-  #         end
-  #       end
+      # owner of the post in the board should be able to update it
+      it "return http unauthorized" do
+        put api_board_post_path(@shared_board, @shared_post), as: :json
+        expect(response).to have_http_status(:unauthorized)
+      end
 
-  #       # while the shared user should not
-  #       describe "Shared user" do
-  #         it "cannot update the post" do
-  #           put api_board_post_path(@shared_board, @shared_post), headers: auth_headers(@user_2), params: valid_attributes, as: :json
-  #           expect(response).to have_http_status(:forbidden)
-  #           @shared_post.reload
-  #           expect(@shared_post.title).to_not eq(valid_attributes[:title])
-  #           expect(@shared_post.content).to_not eq(valid_attributes[:content])
-  #         end
-  #       end
-  #     end
+      context "Post owner" do
+        it "return http unprocessable_entity when invalid title" do
+          put api_board_post_path(@shared_board, @shared_post), headers: auth_headers(@user), params: invalid_title_attribute, as: :json
+          expect(response).to have_http_status(:unprocessable_entity)
+          # todo: assert errors
+        end
+        it "return http unprocessable_entity when invalid content" do
+          put api_board_post_path(@shared_board, @shared_post), headers: auth_headers(@user), params: invalid_content_attribute, as: :json
+          expect(response).to have_http_status(:unprocessable_entity)
+          # todo: assert errors
+        end
+        it "updates my post belonging to my board" do
+          put api_board_post_path(@shared_board, @shared_post), headers: auth_headers(@user), params: valid_attributes, as: :json
+          expect(response).to have_http_status(:ok)
+          expect(response.body).to match_json_schema(:post)
+          @shared_post.reload
+          expect(response.body).to be_json_eql(serialize(@shared_post))
+          expect(parse_json(response.body, 'title')).to eq(valid_attributes[:title])
+          expect(parse_json(response.body, 'content')).to eq(valid_attributes[:content])
+        end
+      end
 
-  #     describe "Delete my post" do
-  #       it "return http unauthorized" do
-  #         delete api_board_post_path(@shared_board, @shared_post)
-  #         expect(response).to have_http_status(:unauthorized)
-  #       end
-  #       describe "Post owner" do
-  #         it "deletes own post" do
-  #           board_posts_count = @shared_board.posts.count
-  #           delete api_board_post_path(@shared_board, @shared_post), headers: auth_headers(@user)
-  #           expect(response).to have_http_status(:ok)
-  #           expect(@shared_board.posts.count).to eq(board_posts_count-1)
-  #           expect(Post.find_by_id(@shared_post.id)).to be_nil
-  #         end
-  #       end
+      # while the shared user should not
+      context "Shared user" do
+        it "cannot update the post" do
+          put api_board_post_path(@shared_board, @shared_post), headers: auth_headers(@user_2), params: valid_attributes, as: :json
+          expect(response).to have_http_status(:forbidden)
+          @shared_post.reload
+          expect(@shared_post.title).to_not eq(valid_attributes[:title])
+          expect(@shared_post.content).to_not eq(valid_attributes[:content])
+        end
+      end
+    end
 
-  #       describe "Shared user" do
-  #         it "return http forbidden" do
-  #           board_posts_count = @shared_board.posts.count
-  #           delete api_board_post_path(@shared_board, @shared_post), headers: auth_headers(@user_2)
-  #           expect(response).to have_http_status(:forbidden)
-  #           expect(board_posts_count).to eq(@shared_board.posts.count)
-  #           expect(Post.find_by_id(@shared_post.id)).not_to be_nil
-  #         end
-  #       end
+    describe "Delete my post" do
 
-  #     end
-  #   end
+      it "return http unauthorized" do
+        delete api_board_post_path(@shared_board, @shared_post)
+        expect(response).to have_http_status(:unauthorized)
+      end
 
-  # end
+      describe "Post owner" do
+        it "deletes own post" do
+          board_posts_count = @shared_board.posts.count
+          delete api_board_post_path(@shared_board, @shared_post), headers: auth_headers(@user)
+          expect(response).to have_http_status(:ok)
+          expect(@shared_board.posts.count).to eq(board_posts_count-1)
+          expect(Post.find_by_id(@shared_post.id)).to be_nil
+        end
+      end
+
+      describe "Shared user" do
+        it "return http forbidden" do
+          board_posts_count = @shared_board.posts.count
+          delete api_board_post_path(@shared_board, @shared_post), headers: auth_headers(@user_2)
+          expect(response).to have_http_status(:forbidden)
+          expect(board_posts_count).to eq(@shared_board.posts.count)
+          expect(Post.find_by_id(@shared_post.id)).not_to be_nil
+        end
+      end
+
+    end
+  end
 end
