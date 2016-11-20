efdfc3450ac5e6a0662837e0ad68fd8430483041
Adones Pitogo <pitogo.adones@gmail.com>
2016-11-20 18:06:04 +0800

    clean up a bit

--- app/controllers/api/boards_controller.rb
+++ app/controllers/api/boards_controller.rb
@@ -29,49 +29,15 @@ class Api::BoardsController < ApplicationController
   #
   # `POST /api/boards`
   #
-  # Param format
-  # ```
-  # {name: "sting"}
-  # ```
-  #
-  # Success return format:
-  # ```
-  # {
-  #   is_admin: true,
-  #   board: {
-  #     id: 1,
-  #     name: 'board name',
-  #     created_at: '..',
-  #     updated_at: '..'
-  #   }
-  # }
   # ```
   def create
-  	@user_board = UserBoard.new(user_id: @user.id, encrypted_password: params[:encrypted_password])
-  	if @user_board.create_with_board(params[:board][:name])
-  		render json: @user_board, status: :created
-  	else
-  		render json: @user_board.errors.full_messages, status: 422
-  	end
+    @user_board = UserBoard.new(user_id: @user.id, encrypted_password: params[:encrypted_password])
+    if @user_board.create_with_board(params[:board][:name])
+      render json: @user_board, status: :created
+    else
+      render json: @user_board.errors.full_messages, status: 422
+    end
 
-    # @board = Board.new(params.require(:board).permit(:name))
-    # if @board.save
-    #   @user_board = UserBoard.new({
-    #                                 user_id: @user.id,
-    #                                 board_id: @board.id,
-    #                                 encrypted_password: params[:encrypted_password],
-    #                                 is_admin: true
-    #   })
-    #   if @user_board.save
-    #     render json: @user_board, status: :created
-    #   else
-    #     @board.destroy
-    #     render json: @user_board.errors, status: 422
-    #   end
-
-    # else
-    #   render json: @board.errors.full_messages, status: 422
-    # end
   end
 
   # Render a single board.
@@ -111,12 +77,12 @@ class Api::BoardsController < ApplicationController
   # }
   # ```
   def update
-  	if @user_board.update_password_and_board(params[:board][:name], params[:encrypted_password])
-  		render json: @user_board
-  	else
-  		errors = @board.errors.full_messages.concat @user_board.errors.full_messages
-  		render json: errors, status: 422
-  	end
+    if @user_board.update_password_and_board(params[:board][:name], params[:encrypted_password])
+      render json: @user_board
+    else
+      errors = @board.errors.full_messages.concat @user_board.errors.full_messages
+      render json: errors, status: 422
+    end
   end
 
   # Deletes a board resource.
