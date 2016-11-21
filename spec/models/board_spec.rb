d98dfaecf99a74d81395caa701999b4c8933b420
Adones Pitogo <pitogo.adones@gmail.com>
2016-11-22 04:20:56 +0800

    spec for validations

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -2,6 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Board, type: :model do
 
+  let(:invalid_attributes) {
+    {
+      name: ''
+    }
+  }
+
   it_behaves_like 'encryptable', [:name]
+
+  it "validates name" do
+    board = Board.new(invalid_attributes)
+    board.save
+    expect(board.errors.count).to be 1
+  end
 
 end
