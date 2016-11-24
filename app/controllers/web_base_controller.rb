61b54dbfc3dff6303ace3651f4f77a5480e26795
Adones Pitogo <pitogo.adones@gmail.com>
2016-11-25 02:05:47 +0800

    render welcome page

--- app/controllers/web_base_controller.rb
+++ app/controllers/web_base_controller.rb
@@ -4,6 +4,10 @@ class WebBaseController < ActionController::Base
   include ActionController::ImplicitRender
   include ActionView::Layouts
 
+  # Renders welcome page
+  def index
+  end
+
   # Renders the angular index view when request url is /app/* to enable html5 pushState capability of angularjs
   def app
     render file: "#{Rails.root}/public/app/index.html", layout: false
