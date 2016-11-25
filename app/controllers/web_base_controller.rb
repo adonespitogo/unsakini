8c40c0314436e59d58d0d5a7c280c8bdc4726a44
Adones Pitogo <pitogo.adones@gmail.com>
2016-11-25 21:41:03 +0800

    move dist to gem root public dir

--- app/controllers/web_base_controller.rb
+++ app/controllers/web_base_controller.rb
@@ -18,6 +18,8 @@ class WebBaseController < ActionController::Base
 
   # Renders the angular index view when request url is /app/* to enable html5 pushState capability of angularjs
   def app
-    render file: "#{Rails.root}/public/app/index.html", layout: false
+    gem_root = File.expand_path '../../..', __FILE__
+
+    render file: "#{gem_root}/public/app/index.html", layout: false
   end
 end
