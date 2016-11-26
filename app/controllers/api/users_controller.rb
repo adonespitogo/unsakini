a0c2185018ae8405d1c64477f6c42500f917274e
Adones Pitogo <pitogo.adones@gmail.com>
2016-11-27 02:26:33 +0800

    clean up

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -58,15 +58,4 @@ class Api::UsersController < ApiBaseController
     params.permit(:name, :email, :password, :password_confirmation)
   end
 
-  def login
-    user = User.find_by(email: params[:email].to_s.downcase)
-
-    if user && user.authenticate(params[:password])
-      auth_token = JsonWebToken.encode({user_id: user.id})
-      render json: {auth_token: auth_token}, status: :ok
-    else
-      render json: ['Invalid username / password'], status: :unauthorized
-    end
-  end
-
 end
