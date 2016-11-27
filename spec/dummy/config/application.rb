fec0c3218d101e50d66659fd3ba0b2f5aa3d87b0
Adones Pitogo <pitogo.adones@gmail.com>
2016-11-27 21:14:42 +0800

    remove letter_opener from dummy

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -7,7 +7,6 @@ require "unsakini"
 
 module Dummy
   class Application < Rails::Application
-    config.action_mailer.delivery_method = :letter_opener
     config.action_mailer.default_url_options = { :host => "localhost:3000" }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
