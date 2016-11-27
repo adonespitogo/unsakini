947a3182e9904d5cb75aebb5a5b1e83c10326e42
Adones Pitogo <pitogo.adones@gmail.com>
2016-11-27 22:42:56 +0800

    validate confirmed email

--- angular/src/app/login/login.component.ts
+++ angular/src/app/login/login.component.ts
@@ -1,11 +1,7 @@
 
 import {Component} from '@angular/core';
 import {Router} from '@angular/router';
-import {Response} from '@angular/http';
 import {ICredentials, LoginService} from './login.service';
-import {Observable} from 'rxjs/Rx';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/catch';
 
 
 @Component({
@@ -28,29 +24,17 @@ export class LoginComponent {
      e.stopPropagation();
     this.error = null;
     this.loginService.login(this.creds)
-    .catch(this.loginFailedHandler(this))
     .subscribe(
       (json) => {
         this.success = 'Login successful. Redirecting...';
         window.localStorage.setItem('auth_token', json.token);
         // this.router.navigate(['/dashboard']);
         // window.location.assign('/dashboard');
+      },
+      (e) => {
+        this.error = e;
       }
     );
     return false;
-  }
-
-  private loginFailedHandler (self: LoginComponent) {
-    return (err: Response) => {
-      console.log(err.headers.keys())
-      self.error = err.toString()
-
-      if (err.status === 404 && err.text.length === 0)
-        self.error = `Invalid email and password combination.`
-      else
-        self.error = `Uknown error occured. Please file a bug report at https://github.com/adonespitogo/unsakini/issues`
-
-      return Observable.throw(err);
-    };
   }
 }
