1ba64b098543ce17b96add39d3a61b74174e3b80
Adones Pitogo <pitogo.adones@gmail.com>
2016-11-28 01:57:10 +0800

    confirm account

---
+++ angular/src/app/confirm-account/confirm-account.service.ts
@@ -0,0 +1,27 @@
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import {HttpService} from '../services';
+
+@Injectable()
+
+export class ConfirmAccountService {
+  constructor(private http: HttpService) { }
+
+  confirm (token: string): Observable<any> {
+    return this.http.get(`/user/confirm/${token}`).map((res) => {
+      console.log(res)
+      if (res.ok) {
+        return {confirmed: true}
+      } else {
+        return {confirmed: false}
+      }
+    }).catch(this.catchError);
+  }
+
+  private catchError(res) {
+    return Observable.of({confirmed: false});
+  }
+
+}
