10d849739e90924b421eb916ca8067a6d76e0007
Adones Pitogo <pitogo.adones@gmail.com>
2016-11-29 07:37:14 +0800

    todo: fix confirmation link

--- app/mailers/unsakini/user_mailer.rb
+++ app/mailers/unsakini/user_mailer.rb
@@ -3,7 +3,7 @@ class Unsakini::UserMailer < ActionMailer::Base
 
     def confirm_account(user)
       @user = user
-      @url  = "#{root_url}unsakini/app/account/confirm/#{@user.confirmation_token}"
+      # @url  = "#{root_url}unsakini/app/account/confirm/#{@user.confirmation_token}"
       mail(to: @user.email, subject: 'Unsakini - Account Confirmation')
     end
 end
